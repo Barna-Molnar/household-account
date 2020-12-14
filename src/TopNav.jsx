dc01c576ff438ce1fd6167107eac2c681f069e1e
Barnabas Molnar <hanta911@gmail.com>
2020-12-14 09:47:41 +0100

    Creating TopNav

--- src/TopNav.jsx
+++ src/TopNav.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 const TopNav = () => {
-  return <nav></nav>;
+  return (
+    <nav>
+      <p>Welcome in our HouseHold Account</p>
+      <img src="#" alt="Family-logo" />
+      <form className="login" action="">
+        <input
+          type="text"
+          placeholder="user"
+          class="login__input login__input--user"
+        />
+
+        <input
+          type="password"
+          placeholder="PIN"
+          maxlength="4"
+          class="login__input login__input--pin"
+        />
+        <button class="login__btn">&rarr;</button>
+      </form>
+    </nav>
+  );
 };
 
 export default TopNav;
